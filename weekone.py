5caa387b2987a45aff676f1c9b68f1eb1a637393
Francis <francis.lin@tksping.com>
2024-10-11 16:57:22 +0800

    update array, two pointers

---
+++ weekone.py
@@ -0,0 +1,111 @@
+list_one = [1, 2, 3, 4, 5]
+
+dict_one = {
+    "name": "John",
+}
+
+set_one = set()  # lookup is constant time O(1) (worse), Theta is average time complexity
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        # set_nums = set(nums) # O(n) is from this.
+        #
+        # if len(set_nums) != len(nums):
+        #    return True
+        # else:
+        #    return False
+
+        # Standard solution
+        set_nums = set()
+
+        for n in nums:
+            if n in set_nums:
+                return True
+
+            set_nums.add(n)
+
+        return False
+
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+
+        # else is not prefferred
+
+        hash_map_s = {}
+        hash_map_t = {}
+
+        def create_hash_map(s, map):
+            for char in s:
+                if char in map:
+                    map[char] += 1
+                    continue
+
+                map[char] = 1
+
+        create_hash_map(s, hash_map_s)
+        create_hash_map(t, hash_map_t)
+
+        if hash_map_s == hash_map_t:
+            return True
+        return False
+
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # O(1)
+        # array 8, 16 bite for one position, but string
+        # O(n)
+        pString = ""
+        for char in s:
+            if not char.isalnum():
+                continue
+            pString += char.lower()
+
+        lPtr = 0
+        rPtr = len(pString) - 1
+
+        while lPtr < rPtr:
+            leftChar = pString[lPtr]
+            rightChar = pString[rPtr]
+
+            if leftChar != rightChar:
+                return False
+
+            lPtr += 1
+            rPtr -= 1
+
+        return True
+
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        leftPtr = 0
+        rightPtr = 0
+        profit = 0
+
+        while rightPtr < len(prices):
+            if prices[leftPtr] > prices[rightPtr]:
+                leftPtr = rightPtr  # template
+
+            profit = max(prices[rightPtr] - prices[leftPtr], profit)
+            rightPtr += 1  # template
+
+        return profit
