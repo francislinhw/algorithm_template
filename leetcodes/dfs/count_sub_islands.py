c1fffa5a90fb397afc1b404a85fb03358cfd041d
Francis <francis.lin@tksping.com>
2025-03-23 14:42:34 -0400

    update

--- leetcodes/dfs/count_sub_islands.py
+++ leetcodes/dfs/count_sub_islands.py
@@ -1,4 +1,100 @@
 # https://leetcode.com/problems/count-sub-islands/
+from typing import List
+
+
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        # 1.45 # Bad
+        # Step 1: Iterate Grid 1 and have visted
+        # Step 2: Iterate Grid 2 and have isolated island
+        # Step 3: Check if the island in the grid2 is all in grid 1
+
+        xLen = len(grid1[0])
+        yLen = len(grid1)
+
+        visited = set()
+        visitedg2 = set()
+        visitedForLoop = set()
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def dfs(x, y):
+            if (x, y) in visited or x < 0 or y < 0 or x >= xLen or y >= yLen:
+                return
+            if grid1[y][x] == 0:
+                return
+            visited.add((x, y))
+            for dx, dy in directions:
+                dfs(x + dx, y + dy)
+
+        for i in range(xLen):
+            for j in range(yLen):
+                if (i, j) not in visited and grid1[j][i] == 1:
+                    dfs(i, j)
+
+        def dfsg2(x, y):
+            if (x, y) in visitedForLoop or x < 0 or y < 0 or x >= xLen or y >= yLen:
+                return
+            if grid2[y][x] == 0:
+                return
+            visitedg2.add((x, y))
+            visitedForLoop.add((x, y))
+            for dx, dy in directions:
+                dfsg2(x + dx, y + dy)
+
+        grid2Islands = []
+
+        for i in range(xLen):
+            for j in range(yLen):
+                if (i, j) not in visitedForLoop and grid2[j][i] == 1:
+                    dfsg2(i, j)
+                    grid2Islands.append(list(visitedg2))
+                    visitedg2 = set()
+
+        result = 0
+
+        for island in grid2Islands:
+            isAllIn = True
+            for isld in island:
+                if isld not in list(visited):
+                    isAllIn = False
+            if isAllIn:
+                result += 1
+
+        return result
+
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+
+        yLen, xLen = len(grid1), len(grid1[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+
+        def dfs(x, y):
+            if (x, y) in visited or x < 0 or y < 0 or x >= xLen or y >= yLen:
+                return True
+            if grid2[y][x] == 0:
+                return True  # 不影響 sub-island 判斷
+
+            visited.add((x, y))
+
+            # 只要 grid2 是陸地，但 grid1 是水，那就不是 sub-island
+            is_sub = grid1[y][x] == 1
+
+            for dx, dy in directions:
+                is_sub = dfs(x + dx, y + dy) and is_sub  # 必須所有子點都合法
+
+            return is_sub
+
+        count = 0
+        for y in range(yLen):
+            for x in range(xLen):
+                if grid2[y][x] == 1 and (x, y) not in visited:
+                    if dfs(x, y):
+                        count += 1
+
+        return count
+
+
 class Solution(object):
     def countSubIslands(self, grid1, grid2):
         """
@@ -9,7 +105,7 @@ class Solution(object):
 
         if not grid1 or not grid2:
             return 0
-        
+
         rLen = len(grid1)
         cLen = len(grid1[0])
 
@@ -17,40 +113,59 @@ class Solution(object):
 
         for r in range(rLen):
             subGrid[r] = [0] * cLen
-        
 
         for r in range(rLen):
             for c in range(cLen):
                 subGrid[r][c] = 1 if grid1[r][c] == 1 and grid2[r][c] == 1 else 0
-        
+
         numberIsland = 0
 
         visitedSet = set()
 
         def visitWholeIsland(r, c):
-            if (r, c) in visitedSet or r < 0 or r >= rLen or c < 0 or c >= cLen or subGrid[r][c] == 0:
+            if (
+                (r, c) in visitedSet
+                or r < 0
+                or r >= rLen
+                or c < 0
+                or c >= cLen
+                or subGrid[r][c] == 0
+            ):
                 return False
 
             visitedSet.add((r, c))
 
-            visitWholeIsland(r+1, c)
-            visitWholeIsland(r, c+1)
-            visitWholeIsland(r-1, c)
-            visitWholeIsland(r, c-1)
+            visitWholeIsland(r + 1, c)
+            visitWholeIsland(r, c + 1)
+            visitWholeIsland(r - 1, c)
+            visitWholeIsland(r, c - 1)
 
             return True
-        
+
         for r in range(rLen):
             for c in range(cLen):
                 if (r, c) in visitedSet:
                     continue
                 if visitWholeIsland(r, c):
                     numberIsland += 1
-        
+
         return numberIsland
-    
-grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]]
-grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]
+
+
+grid1 = [
+    [1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1],
+]
+grid2 = [
+    [1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+]
 expected = 3
 
 s = Solution()
--- leetcodes/dfs/island_perimeter.py
+++ leetcodes/dfs/island_perimeter.py
@@ -1,5 +1,57 @@
 # https://leetcode.com/problems/island-perimeter/
 
+from typing import List
+
+# 23 March 2025
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # 12.46
+
+        # Step 1: Find one part of the island
+        # Step 2: DFS iterate the island and also count the perimeter
+        xLen = len(grid[0])
+        yLen = len(grid)
+
+        perimeter = [0]
+
+        visited = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def dfs(x, y):
+            if (x, y) in visited or x < 0 or y < 0 or x >= xLen or y >= yLen:
+                return
+            if grid[y][x] == 0:
+                return
+            visited.add((x, y))
+
+            for dx, dy in directions:
+                neighborX = x + dx
+                neighborY = y + dy
+                if (
+                    neighborX < 0
+                    or neighborY < 0
+                    or neighborX >= xLen
+                    or neighborY >= yLen
+                ):
+                    perimeter[0] += 1
+                elif grid[neighborY][neighborX] == 0:
+                    perimeter[0] += 1
+                else:
+                    perimeter[0] += 0
+
+            for dx, dy in directions:
+                dfs(x + dx, y + dy)
+
+        for i in range(xLen):
+            for j in range(yLen):
+                if grid[j][i] == 1 and (i, j) not in visited:
+                    dfs(i, j)
+
+        return perimeter[0]  # 20 Mins
+
+
 class Solution(object):
     def islandPerimeter(self, grid):
         """
@@ -9,7 +61,7 @@ class Solution(object):
 
         if not grid:
             return 0
-        
+
         # r * c
         rGrid = len(grid)
         cGrid = len(grid[0])
@@ -22,33 +74,39 @@ class Solution(object):
             if r < 0 or r >= rGrid or c < 0 or c >= cGrid or grid[r][c] == 0:
                 return True
 
-
         def countingPerimeter(r, c):
-            if (r, c) in visitedSet or r < 0 or r >= rGrid or c < 0 or c >= cGrid or grid[r][c] == 0:
+            if (
+                (r, c) in visitedSet
+                or r < 0
+                or r >= rGrid
+                or c < 0
+                or c >= cGrid
+                or grid[r][c] == 0
+            ):
                 return 0
-            
+
             visitedSet.add((r, c))
 
             side = 0
-            side += 1 if isSeaOrEdge(r, c-1) == True else 0
-            side += 1 if isSeaOrEdge(r, c+1) == True else 0
-            side += 1 if isSeaOrEdge(r-1, c) == True else 0
-            side += 1 if isSeaOrEdge(r+1, c) == True else 0
+            side += 1 if isSeaOrEdge(r, c - 1) == True else 0
+            side += 1 if isSeaOrEdge(r, c + 1) == True else 0
+            side += 1 if isSeaOrEdge(r - 1, c) == True else 0
+            side += 1 if isSeaOrEdge(r + 1, c) == True else 0
 
-            upper = countingPerimeter(r, c-1)
-            lower = countingPerimeter(r, c+1)
-            left = countingPerimeter(r-1, c)
-            right = countingPerimeter(r+1, c)
+            upper = countingPerimeter(r, c - 1)
+            lower = countingPerimeter(r, c + 1)
+            left = countingPerimeter(r - 1, c)
+            right = countingPerimeter(r + 1, c)
 
             return side + upper + lower + left + right
-        
+
         perimeter = 0
 
         for r in range(rGrid):
             for c in range(cGrid):
                 if (r, c) in visitedSet:
                     continue
-                
+
                 perimeter += countingPerimeter(r, c)
 
         return perimeter
@@ -59,7 +117,7 @@ class Solution:
 
         if not grid:
             return 0
-        
+
         visited = set()
         rowLen = len(grid)
         colLen = len(grid[0])
@@ -70,13 +128,11 @@ class Solution:
             if (r, c) in visited:
                 return 0
             visited.add((r, c))
-            top = dfs(r-1, c)
-            down = dfs(r+1, c)
-            right = dfs(r, c+1)
-            left = dfs(r, c-1)
+            top = dfs(r - 1, c)
+            down = dfs(r + 1, c)
+            right = dfs(r, c + 1)
+            left = dfs(r, c - 1)
             return top + down + left + right
-
-
 
         for i in range(rowLen):
             for j in range(colLen):
